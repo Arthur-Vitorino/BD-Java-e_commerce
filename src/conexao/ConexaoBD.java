ab748071a78db5d535202dca991fd7523c6acfe5
Anna Julia Curcio Marques <134973253+annaju0405@users.noreply.github.com>
2024-10-31 15:19:06 -0300

    Update ConexaoBD.java

--- src/conexao/ConexaoBD.java
+++ src/conexao/ConexaoBD.java
@@ -1,21 +1,23 @@
 package conexao;
-
+//A classe ConexaoBD é responsável por gerenciar a conexão com o banco de dados.
+//Utiliza o driver JDBC para estabelecer uma conexão com o banco MySQL.
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBD {
-
+// URL de conexão ao banco de dados MySQL
     private static final String URL = "jdbc:mysql://localhost:3306/e_commerce";
-    private static final String USER = "root";
-    private static final String PASSWORD = "catolica";
+    private static final String USER = "root";  // Nome de usuário para acessar o banco de dados
+    private static final String PASSWORD = "catolica";  // Senha para acessar o banco de dados
 
     public static Connection getConnection() throws SQLException {
-        try {
+        try { // Registra o driver JDBC do MySQL
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(URL, USER, PASSWORD); // Retorna a conexão com o banco de dados
         } catch (ClassNotFoundException e) {
-            throw new SQLException("Driver JDBC não encontrado", e);
+            throw new SQLException("Driver JDBC não encontrado", e); // Lança uma SQLException se o driver não for encontrado
         }
     }
 }
+// Comentario feito por Anna Júlia 
